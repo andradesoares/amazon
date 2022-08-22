b32e03dca377845f7b8df901ba102cc34130b137
Daniel Soares <danielandradesoares@hotmail.com>
2022-08-22 15:58:11 -0300

    Basic crud

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,14 +1,50 @@
-import { Body, Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ProductDocument } from './product.schema';
+import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
-  constructor() {}
+  constructor(private productService: ProductService) {}
+
   @Post()
   createProduct(
     @Body('name') name: string,
-    @Body('price') price: string,
+    @Body('price') price: number,
     @Body('description') description?: string,
-  ) {
-    return this.productService.createProduct(name, price, description);
+  ): Promise<ProductDocument> {
+    return this.productService.create(name, price, description);
+  }
+
+  @Get()
+  findAllProducts(): Promise<ProductDocument[]> {
+    return this.productService.findAll();
+  }
+
+  @Get(':id')
+  findOneProduct(@Param('id') id: string): Promise<ProductDocument> {
+    return this.productService.findOne(id);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description?: string,
+  ): Promise<ProductDocument> {
+    return this.productService.update(id, name, price, description);
+  }
+
+  @Delete()
+  deleteProduct(@Param('id') id: string) {
+    return this.productService.delete(id);
   }
 }
