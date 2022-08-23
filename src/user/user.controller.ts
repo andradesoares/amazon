a2c7434265018400741d5baecf9f24b8c65aa838
Daniel Soares <danielandradesoares@hotmail.com>
2022-08-23 16:05:27 -0300

    user controller get method implementation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserInterface } from './user.interface';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get(':id')
+  getUser(@Param('id') id: string): Promise<UserInterface> {
+    return this.userService.findById(id);
+  }
+}
