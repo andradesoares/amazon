52e5206ebe6591b9a9ab965d42070e9d79644219
Daniel Soares <danielandradesoares@hotmail.com>
2022-08-23 11:42:38 -0300

    auth module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +12,8 @@ import { ProductModule } from './product/product.module';
       'mongodb+srv://amazon:4EC3E4RM76kbNwrE@cluster0.4wra4qw.mongodb.net/?retryWrites=true&w=majority',
     ),
     ProductModule,
+    UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
