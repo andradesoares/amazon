5997320daabf3573a68a60ff6610f08ecc873494
Daniel Soares <danielandradesoares@hotmail.com>
2022-08-23 15:11:15 -0300

    user singup method implementation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserSignUpDTO } from '../user/dtos/signup-user.dto';
+import { UserInterface } from '../user/user.interface';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signUp(@Body() user: UserSignUpDTO): Promise<UserInterface | any> {
+    return this.authService.signup(user);
+  }
+}
