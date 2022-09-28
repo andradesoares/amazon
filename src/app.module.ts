c728b4874f6f1fb1a7a715a496b6fe5bbe9861cb
Daniel Soares <danielandradesoares@hotmail.com>
2022-09-28 14:29:14 -0300

    checkout controller method

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,13 @@ import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { StripeModule } from './stripe/stripe.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(
       'mongodb+srv://amazon:4EC3E4RM76kbNwrE@cluster0.4wra4qw.mongodb.net/?retryWrites=true&w=majority',
     ),
